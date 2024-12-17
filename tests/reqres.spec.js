cee8534c7b29965434ed7a792975a8366f1aa38a
Vasu <faculty-tech@facultydigital.com>
2024-12-17 13:17:31 +0700

    Add AJV schema validation for API response

--- tests/reqres.spec.js
+++ tests/reqres.spec.js
@@ -1,9 +1,14 @@
 const { test, expect, request } = require('@playwright/test');
+const { Ajv } = require('ajv');
+let ajv = new Ajv();
 
 test.describe('Reqres API GET', () => {
   test('get single user', async ({ request }) => {
     let response = await request.get('/api/users/2');
     let body = await response.json();
+    let valid = ajv.validate(require('../json-schema/get-user.schema.json'), body);
+    if (!valid) console.log(ajv.errors);
+    expect(valid).toBe(true);
     expect(response.status()).toBe(200);
     expect(body.data.id).toBe(2);
   });
@@ -20,11 +25,15 @@ test.describe('Reqres API POST', () => {
     };
     let response = await request.post('/api/users', {
       headers: headers,
-      body: body
+      data: body
     });
+    let bodyResponse = await response.json();
     expect(response.status()).toBe(201);
-    expect(body.name).toBe('morpheus');
-    expect(body.job).toBe('leader');
+    expect(bodyResponse.job).toBe('leader');
+    expect(bodyResponse.name).toBe('morpheus');
+    let valid = ajv.validate(require('../json-schema/create-user.schema.json'), bodyResponse);
+    if (!valid) console.log(ajv.errors);
+    expect(valid).toBe(true);
   });
 });
 
@@ -39,17 +48,27 @@ test.describe('Reqres API PUT', () => {
     };
     let response = await request.put('/api/users/2', {
       headers: headers,
-      body: body
+      data: body
     });
+    let bodyResponse = await response.json();
     expect(response.status()).toBe(200);
-    expect(body.name).toBe('morpheus');
-    expect(body.job).toBe('zion resident');
+    expect(bodyResponse.job).toBe('zion resident');
+    expect(bodyResponse.name).toBe('morpheus');
+    let valid = ajv.validate(require('../json-schema/update-user.schema.json'), bodyResponse);
+    if (!valid) console.log(ajv.errors);
+    expect(valid).toBe(true);
   });
 });
 
 test.describe('Reqres API DELETE', () => {
   test('delete user', async ({ request }) => {
     let response = await request.delete('/api/users/2');
+    let body = await response.body();
+    expect(body).toHaveLength(0);
     expect(response.status()).toBe(204);
+    let valid = ajv.validate(require('../json-schema/delete-user.schema.json'), body);
+    if (!valid) console.log(ajv.errors);
+    expect(valid).toBe(true);
+
   });
 });
